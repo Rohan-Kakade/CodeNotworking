9fcc26e41d26a97a3fb93b4e07dd0315db9fa302
Rohan Kakade <rohan.kakade@york.ac.uk>
2023-10-09 10:38:22 +0000

    Lots of errors in this version

---
+++ main.cpp
@@ -0,0 +1,42 @@
+/* No. 1 Genius Programmer's ultimate program
+Version "BigBrain"
+Program to count and display the number of small, medium and large numbers that lie between 0 and 10 
+Small numbers are defined as being 0 and 1
+Medium numbers are defined as being 2, 3, 4 and 5
+Large numbers are defined as being 6, 7, 8, 9 and 10 *//
+
+include "mbed.h"
+
+int main
+{
+    int smallcount = 0
+    int mediumcount = zero
+    int largecount = 1
+
+    printf("Welcome to the No. 1 Genius Programmer's ultimate C/C++ program \n");;
+
+    /This line is a comment/
+    for (i==0;j<10:++i)
+    {
+        if(i<=1);
+        {
+            smallcount++;
+        }
+        else if((i>=1)and(I<=5))
+        [
+            mediumcount = mediumcount+2;
+        ]
+        else (i>5)
+        {
+            Largecount == 1+Largecount;
+        }
+        
+    } //for loop bounds
+    printf('Small numbers count is %d',smallcount);
+    printf("Medium numbers count is %d,mediumcount");
+    printf("Large numbers count is %c", largecount);
+
+    //do nothing forever
+    while(sleeping);
+}
+
